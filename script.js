263086d93e3d3c8b1172245919db351e558f807a
Michael Drew Palazola <mpalazola@hotmail.com>
2019-06-21 14:28:56 -0500

    uploading content

---
+++ script.js
@@ -0,0 +1,55 @@
+let outputEl = document.getElementById("output-target")
+let articleEl = document.getElementsByClassName("article-section")
+let header = document.querySelector("#page-header")
+
+function handleSectionClick (event) {
+    console.log(event)
+}
+
+for (let i = 0; i < articleEl.length; i++) {
+    articleEl[i].addEventListener("click", handleSectionClick)
+}
+
+function handleHeaderMouseOver (event) {
+    outputEl.innerHTML = "You moved your mouse over me"
+}
+
+function handleHeaderMouseOut(event) {
+    outputEl.innerHTML = "Why u leave me?"
+}
+
+header.addEventListener("mouseover", handleHeaderMouseOver)
+header.addEventListener("mouseout", handleHeaderMouseOut)
+
+let fieldEl = document.getElementById("keypress-input")
+
+fieldEl.addEventListener("keyup", function (event) {
+    outputEl.innerHTML = event.target.value
+})
+
+let guineaPig = document.getElementById("guinea-pig")
+
+function toggleClass (newClass) {
+    guineaPig.classList.toggle(newClass)
+    console.log("guineaPig.classList", guineaPig.classList)
+  }
+
+document.getElementById("add-color").addEventListener("click", function() {
+    toggleClass("blue")
+})
+
+document.getElementById("make-large").addEventListener("click", function() {
+    toggleClass("large")
+})
+
+document.getElementById("add-border").addEventListener("click", function() {
+    toggleClass("bordered")
+})
+
+document.getElementById("add-rounding").addEventListener("click", function() {
+    toggleClass("rounded")
+})
+
+
+
+
